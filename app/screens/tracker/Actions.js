ae5cf836616fa988222dd447b095b74ee5c6b6a3
rodogir <me@rodogir.com>
2016-09-01 22:13:38 +0200

    actions to start and stop timelog

---
+++ app/screens/tracker/Actions.js
@@ -0,0 +1,25 @@
+import uuid from "uuid";
+import moment from "moment";
+
+export const START_TIME_LOG = "START_TIME_LOG";
+export const STOP_TIME_LOG = "STOP_TIME_LOG";
+
+export function createTimeLog() {
+  return {
+    type: START_TIME_LOG,
+    payload: {
+      id: uuid.v4(),
+      startDate: moment().toISOString()
+    }
+  }
+}
+
+export function stopTimeLog(id) {
+  return {
+    type: STOP_TIME_LOG,
+    payload: {
+      id,
+      stopDate: moment().toISOString()
+    }
+  }
+}
